f485d299b95b2757226b95f50682e253662fe90e
Rutujasnp <65347296+Rutujasnp@users.noreply.github.com>
2024-10-24 02:10:56 -0700

    implemnted logic to pass single number string test case

--- src/main/java/com/testDemo/TestDemoTDD/CalculatorService.java
+++ src/main/java/com/testDemo/TestDemoTDD/CalculatorService.java
@@ -2,15 +2,13 @@ package com.testDemo.TestDemoTDD;
 
 public class CalculatorService {
 
-	
-	public static int  addNums(String numbers) {
-	
+	public static int addNums(String numbers) {
+
+		if (numbers.isEmpty()) {		//for if the string numbers is empty
+			return 0;
+		}
+		int result = Integer.parseInt(numbers);		// for single number in string numbers
+		return result;
 		
-//		if(numbers.isEmpty()) {
-//			return 0;
-//		}
-//		int result=Integer.parseInt(numbers);
-//		return result;
-		return 0;
 	}
 }
