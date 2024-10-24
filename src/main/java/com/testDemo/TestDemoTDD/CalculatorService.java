41c66dd104d0f54a12ac758cc24aaa1974e9b0ec
Rutujasnp <65347296+Rutujasnp@users.noreply.github.com>
2024-10-24 01:55:05 -0700

    initial commit

---
+++ src/main/java/com/testDemo/TestDemoTDD/CalculatorService.java
@@ -0,0 +1,12 @@
+package com.testDemo.TestDemoTDD;
+
+public class CalculatorService {
+
+	
+	public static int  addNums(String numbers) {
+	
+//		int result=Integer.parseInt(numbers);
+		//return result;
+		return 0;
+	}
+}
---
+++ src/test/java/com/testDemo/TestDemoTDD/TestDemoTddApplicationTests.java
@@ -0,0 +1,20 @@
+package com.testDemo.TestDemoTDD;
+
+import org.assertj.core.api.Assert;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDemoTddApplicationTests {
+	@Test
+	public void testEmptyString() {
+		Assertions.assertEquals(0, CalculatorService.addNums(" "));
+
+}
+//	@Test
+//	public void testSingleNum() {
+//		Assertions.assertEquals(6, CalculatorService.addNums("6"));
+//	}
+}
