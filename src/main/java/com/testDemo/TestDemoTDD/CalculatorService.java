f5e4906f1b51878e7c9bf678aa9951c1f7ab54a9
Rutujasnp <65347296+Rutujasnp@users.noreply.github.com>
2024-10-24 02:42:29 -0700

    implmented logic to add two numbers

--- src/main/java/com/testDemo/TestDemoTDD/CalculatorService.java
+++ src/main/java/com/testDemo/TestDemoTDD/CalculatorService.java
@@ -4,11 +4,17 @@ public class CalculatorService {
 
 	public static int addNums(String numbers) {
 
-		if (numbers.isEmpty()) {		//for if the string numbers is empty
+		if (numbers=="null"|| numbers.isEmpty()) {		//for if the string numbers is empty
 			return 0;
 		}
-		int result = Integer.parseInt(numbers);		// for single number in string numbers
-		return result;
+//		int result = Integer.parseInt(numbers);		// for single number in string numbers
+//		return result;
 		
+		String arrayOfNumbers[]= numbers.split(",");
+		int sum=0;
+		for (String str: arrayOfNumbers) {
+			sum+=Integer.parseInt(str.trim());
+		}
+		return sum;
 	}
 }
