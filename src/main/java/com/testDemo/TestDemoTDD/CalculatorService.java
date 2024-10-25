efc868570404d7ea2a79994cabd74e8cff3f7aa2
Rutujasnp <65347296+Rutujasnp@users.noreply.github.com>
2024-10-24 23:26:41 -0700

    implemented logic to pass test for custom delimiter

--- src/main/java/com/testDemo/TestDemoTDD/CalculatorService.java
+++ src/main/java/com/testDemo/TestDemoTDD/CalculatorService.java
@@ -4,19 +4,30 @@ public class CalculatorService {
 
 	public static int addNums(String numbers) {
 
-		if (numbers=="null"|| numbers.isEmpty()) {		//for if the string numbers is empty
+		if (numbers == "null" || numbers.isEmpty()) { // for if the string numbers is empty
 			return 0;
-			
+
 		}
-		
-		numbers= numbers.replace("\n", ",");
-		
-		//to add numbers into sum
-		String arrayOfNumbers[]= numbers.split(",");
-		int sum=0;
-		for (String str: arrayOfNumbers) {
-			sum+=Integer.parseInt(str.trim());
-			
+		// if the string contains custom delimiter
+		String delimiter = ",|\n";
+		int delimiterIndex = numbers.indexOf("\n");
+		if (numbers.startsWith("//")) {
+			delimiter = numbers.substring(2, delimiterIndex);
+			// Handle multiple character delimiters surrounded by []
+			if (delimiter.startsWith("[") && delimiter.endsWith("]")) {
+				delimiter = delimiter.substring(1, delimiter.length() - 1); // Remove the brackets
+			}
+			numbers = numbers.substring(delimiterIndex + 1);
+		}
+
+		// numbers= numbers.replace("\n", ",");
+
+		// to add numbers into sum
+		String arrayOfNumbers[] = numbers.split(delimiter);
+		int sum = 0;
+		for (String str : arrayOfNumbers) {
+			sum += Integer.parseInt(str.trim());
+
 		}
 		return sum;
 	}
